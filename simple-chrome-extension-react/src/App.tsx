586fe1a87df81b6590c470e502b5e39e55613ffd
Caleb O'Malley <calebomalley@ufl.edu>
2024-01-07 07:59:22 -0500

    onclick functionality to popup

--- simple-chrome-extension-react/src/App.tsx
+++ simple-chrome-extension-react/src/App.tsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const onclick = async () => {
+    const [tab] = await chrome.tabs.query({active: true})
+    chrome.scripting.executeScript({
+      target: {tabId: tab.id!},
+      func: () => {
+        alert('Hello from my Vite extension!')
+      }
+    })
+  }
 
   return (
     <>
@@ -18,8 +26,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={onclick}>
+          Alert me!
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
