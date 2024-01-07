8315e5bb638f261508b6bb44a1fa20f00614fbe0
Caleb O'Malley <calebomalley@ufl.edu>
2024-01-07 08:10:51 -0500

    refactored + simplified

--- simple-chrome-extension-react/src/App.tsx
+++ simple-chrome-extension-react/src/App.tsx
@@ -1,41 +1,30 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+
+  const [color, setColor] = useState('')
   
   const onclick = async () => {
     const [tab] = await chrome.tabs.query({active: true})
-    chrome.scripting.executeScript({
+    chrome.scripting.executeScript<string[], void>({
       target: {tabId: tab.id!},
-      func: () => {
-        alert('Hello from my Vite extension!')
+      args: [color],
+      func: (color) => {
+        document.body.style.backgroundColor = color
       }
     })
   }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>BG Color Changer</h1>
       <div className="card">
-        <button onClick={onclick}>
-          Alert me!
+        <input type="color" onChange={(e) => setColor(e.currentTarget.value)} value={color}/>
+        <button onClick={() => onclick()}>
+          Change color!
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
